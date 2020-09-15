1717b8480ffeeec01d948351b08ace1e6909b90d
leo2436 <chenll1996@foxmail.com>
2020-09-15 23:20:34 +0800

    add logout logic

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -12,6 +12,7 @@ import { MyContext } from "src/types";
 import { User } from "../entity/User";
 import argon2 from "argon2";
 import { EntityManager } from "@mikro-orm/postgresql";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -142,5 +143,21 @@ export class UserResolver {
     return {
       user,
     };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log("err: ", err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
   }
 }
